95b505066936e9ecedff09c50f8cf9319dec3125
Viswanadh <viswavishu0110@gmail.com>
2022-03-24 18:35:22 +0530

    adds and auth0 details

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import { FilterProvider } from './context/filter_context'
 import { CartProvider } from './context/cart_context'
 import { UserProvider } from './context/user_context'
 import { Auth0Provider } from '@auth0/auth0-react'
-require('dotenv').config()
+// require('dotenv').config()
 ReactDOM.render(
   <Auth0Provider
-    domain={process.env.REACT_APP_AUTH_DOMAIN}
-    clientId={process.env.REACT_APP_CLIENT_ID}
+    domain="dev-ey8ikaxp.us.auth0.com"
+    clientId="lEb4JjRU6fnY4QVWhidU5r6QC2nH5Got"
     redirectUri={window.location.origin}
     cacheLocation='localstorage'
   >
