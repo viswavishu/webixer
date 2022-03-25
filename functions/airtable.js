9b5655aa7a0007806a6c3c1f706e276f875b2ff6
Viswanadh <viswavishu0110@gmail.com>
2022-03-25 13:15:39 +0530

    adds final api without adding functionality

---
+++ functions/airtable.js
@@ -0,0 +1,32 @@
+//use this when using env variables
+// require('dotenv').config()
+const Airtable = require('airtable-node');
+
+const airtable = new Airtable({ apiKey: 'keyR3sFraWsqJZuo6' })
+    .base('appLJw8rcQWLRrqjC')
+    .table('products')
+
+exports.handler = async (event, context, cb) => {
+    try {
+        const {records} = await airtable.list();
+        // console.log(records);
+        const products = records.map((product) => {
+            const {id} = product
+            const {name, image, price} = product.fields
+            const url = image[0].url
+            return {id, name, url, price}
+        })
+        return {
+            statusCode: 200,
+            // body: 'Airtable Example',
+            body:  JSON.stringify(products),//prints the api to page
+        }
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: 'Internal server error',
+        }
+     }
+   
+}
+
