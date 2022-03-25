f9e52c4d7e97efa215c7d795c4a2efb23c6fa513
Viswanadh <viswavishu0110@gmail.com>
2022-03-25 13:14:44 +0530

    adds few changes for currency conversion

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -8,7 +8,7 @@ exports.handler = async function (event, context) {
     // Replace this constant with a calculation of the order's amount
     // Calculate the order total on the server to prevent
     // people from directly manipulating the amount on the client
-    return shipping_fee + total_amount * 76;
+    return shipping_fee + total_amount;
   }
   try {
     // Create a PaymentIntent with the order amount and currency
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -42,6 +42,7 @@ export const services = [
   },
 ]
 
+
 export const products_url = 'https://course-api.com/react-store-products'
 
 export const single_product_url = `https://course-api.com/react-store-single-product?id=`
